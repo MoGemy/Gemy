ffc14734c131de0d0083e277de09f4ff35b7c15a
islam.ahmed-abdelfattah <eslam-ahmad@outlook.com>
2018-11-20 13:53:49 +0200

    QR Code is now running

--- app/src/main/java/com/example/gemy/test/QRcode/MainActivity.java
+++ app/src/main/java/com/example/gemy/test/QRcode/MainActivity.java
@@ -1,123 +1,61 @@
 package com.example.gemy.test.QRcode;
-//....QR Scanner calss
 
 import android.Manifest;
-import android.content.Context;
-import android.content.pm.PackageManager;
-import android.os.Vibrator;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.SparseArray;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.TextView;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.gemy.test.R;
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.barcode.Barcode;
-import com.google.android.gms.vision.barcode.BarcodeDetector;
+import com.google.zxing.Result;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
-import java.io.IOException;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class MainActivity extends AppCompatActivity {
-    SurfaceView camerapreview;
-    TextView txtresult;
-    BarcodeDetector barcodeDetector;
-    CameraSource cameraSource;
-    final int RequestCameraPermissionID = 1;
+    ZXingScannerView scannerView;
+
+    private RxPermissions rxPermissions;
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case RequestCameraPermissionID: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    try {
-                        cameraSource.start(camerapreview.getHolder());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            break;
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_main );
+        scannerView = findViewById ( R.id.scannerView );
+
+        rxPermissions = new RxPermissions ( this );
+
+        scannerView.setResultHandler ( this::onQrResult );
+
+        rxPermissions
+                .request ( Manifest.permission.CAMERA )
+                .subscribe ( this::onPermissionsResult );
+
+    }
+
+    private void onPermissionsResult( Boolean isGranted ) {
+        if ( isGranted ) {
+            onPermissionGranted ();
+        } else {
+            onPermissionDenied ();
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+    private void onPermissionDenied() {
+        AlertDialog dialog = new AlertDialog.Builder ( this )
+                .setMessage ( "Sorry must use camera to scan QR code, please grant permission in next time" )
+                .setNegativeButton ( "Exit" , ( dialog1 , which ) -> dialog1.dismiss () )
+                .create ();
+        dialog.show ();
+    }
 
-        camerapreview =findViewById(R.id.camerapreview);
-        txtresult =findViewById(R.id.txtResult);
+    private void onPermissionGranted() {
+        scannerView.startCamera ();
+    }
 
+    private void onQrResult( Result result ) {
+        Log.i ( "QR_RESULT" , result.getText () );
 
-        barcodeDetector = new BarcodeDetector
-                .Builder(this)
-                .setBarcodeFormats(Barcode.QR_CODE)
-                .build();
-        cameraSource = new CameraSource
-                .Builder(this, barcodeDetector)
-                .setRequestedPreviewSize(640, 480)
-                .build();
-
-
-        camerapreview.getHolder().addCallback(new SurfaceHolder.Callback() {
-
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                try {
-                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA},RequestCameraPermissionID);
-                        return;
-                    }
-                    cameraSource.start(camerapreview.getHolder());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        cameraSource.stop();
-        }
-    });
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> qrcodes = detections.getDetectedItems();
-                if (qrcodes.size() != 0)
-                {
-                    txtresult.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Vibrator vibrator = (Vibrator)getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
-                            vibrator.vibrate(1000);
-                            txtresult.setText(qrcodes.valueAt(0).displayValue);
-
-                        }
-                    });
-
-                }
-
-            }
-        });
     }
 }
